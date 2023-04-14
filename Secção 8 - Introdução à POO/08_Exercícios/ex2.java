db2c6ae6ce95a1d2b53c59442bdc4ecd751babf6
Tassio <ds.tassio@gmail.com>
2023-04-14 15:27:44 +0100

    08_Exercícios ex2.java completed

--- Secção 8 - Introdução à POO/08_Exercícios/ex2.java
+++ Secção 8 - Introdução à POO/08_Exercícios/ex2.java
@@ -8,31 +8,37 @@ public class ex2 {
         public double price;
         public int quantity;
 
-        public double TotalValueInStock() {
+        public double totalValueInStock() {
             double total_value = (quantity * price);
             return total_value;
         }
 
-        public void AddProducts(int quantity){
+        public void addProducts(int quantity){
             this.quantity += quantity;         // o this está trazendo o quantity da classe produto e adicionando o quantity do método.
         }
 
-        public void RemoveProducts(int quantity){
+        public void removeProducts(int quantity){
             this.quantity -= quantity;
         }
 
         public String toString(){
-            return name;
+            return name
+            + ", $"
+            + String.format("%.2f", price)//formatando o price pra duas casas decimais
+            + ", "
+            + quantity
+            + " units, total: $"
+            + String.format("%.2f", totalValueInStock());
         }
 
     }
 
-    public static void main(String[] args) {
+     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner input = new Scanner(System.in);
 
                                                         // declara a variável prod do tipo Products (método)
-        Product product = new Product();                   // instanciando o método Products
+        Product product = new Product();                // instanciando o método Products
 
         // input variables
         System.out.println("Enter the name of the product: ");
@@ -41,14 +47,20 @@ public class ex2 {
         product.price = input.nextDouble();
         System.out.println("Enter the quantity of products: ");
         product.quantity = input.nextInt();
-        // print the entered variables    
-        System.out.println(product.name + ", " + product.price + ", " + product.quantity);
+        // print the entered variables 1   
+        System.out.println(product.name + ", " + product.price + ", " + product.quantity + "/n");
+        System.out.println("Product data: " + product +"/n");
+        // print the entered variables UPDATE (AddProduct)
+        System.out.println("Enter the number of products to be added in stock: ");
+        int quantity = input.nextInt();                 // entrada de valor na variável quantity para ser inclusa no método AddProduct
+        product.addProducts(quantity);                  // chama a instância product da classe Product e atribui a variável quantity à função AddProduct
+        System.out.println("Update data: " + product +"/n");
+        // print the entered variables REMOVE (RemoveProduct)
+        System.out.println("Enter the number of products to be removed from stock: ");
+        quantity = input.nextInt();                 // entrada de valor na variável quantity para ser inclusa no método RemoveProduct
+        product.removeProducts(quantity);                  // chama a instância product da classe Product e atribui a variável quantity à função RemoveProduct
+        System.out.println("Update data: " + product +"/n");
 
-
-
-
-
-        
 
     }
 }
